913a8aff39e4abab0be75536dbe1defb53687c94
ienming <hsieh.alan.0726@gmail.com>
2025-06-04 21:26:09 +0800

    feat: add Info page

--- pages/info.vue
+++ pages/info.vue
@@ -1,7 +1,6 @@
 <template>
 	<div class="container info">
-		<!-- TODO: 暫時隱藏 -->
-		<!-- <div>
+		<div>
 			<h3 class="mb-space-base">Lucien Hsieh 謝明倫</h3>
 			<p>
 				喜歡結合網頁前端與設計、插畫、資料視覺化方面的興趣。希望能將不同的內容轉譯成有趣的敘事作品。
@@ -9,8 +8,7 @@
 			<p>
 				I use web frontend technologies to express my interests in design, illustration, story, and data visualization.
 			</p>
-		</div> -->
-		<ToBeContinue />
+		</div>
 	</div>
 </template>
 
