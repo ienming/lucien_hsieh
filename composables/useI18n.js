08432e51609d3f33bb165c6812b18f75f83b0a7e
ienming <hsieh.alan.0726@gmail.com>
2026-06-05 23:48:17 +0800

    feat: update favicon, contact greeting

--- composables/useI18n.js
+++ composables/useI18n.js
@@ -16,7 +16,7 @@ const LANG_KEY_MAP = {
 		year: 'Year',
 		no: 'No',
 		medium: 'Medium',
-		get_in_touch_greeting: 'Always open to intriguing ideas in digital storytelling and cultural content design Ü',
+		get_in_touch_greeting: 'Open to intriguing ideas in digital storytelling and cultural content design Ü',
 	},
 	ZH: {
 		fileExplorer: '所有專案',
@@ -28,7 +28,7 @@ const LANG_KEY_MAP = {
 		no: '編號',
 		year: '年份',
 		medium: '媒介',
-		get_in_touch_greeting: '隨時歡迎來自數位敘事、文化內容設計中的有趣想法 Ü',
+		get_in_touch_greeting: '歡迎來自數位敘事、文化內容設計的有趣想法 Ü',
 	},
 }
 
